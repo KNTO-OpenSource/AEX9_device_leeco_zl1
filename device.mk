c922c87e321e09fda5a3b0e485919aa237ca175c
GionYuki <dntln.gionyuki@gmail.com>
2018-12-27 18:03:33 +0900

    update fixup

--- device.mk
+++ device.mk
@@ -25,7 +25,8 @@ $(call inherit-product-if-exists, vendor/leeco/zl1/zl1-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-aosp 
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
